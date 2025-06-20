6ff27638621f81eb2f98d43ed382981d769f70cc
Daanfrie <daanfrie@gmail.com>
2025-06-20 04:26:23 +0200

    test

--- src/main.js
+++ src/main.js
@@ -314,7 +314,7 @@ document.querySelector("#app").innerHTML = `
       <div class="project-card fade-in">
         <div class="relative overflow-hidden rounded-2xl mb-[1.2vw] group">
           <img
-            src="images/ChatbotPortfolio1.png"
+            src="images/OpenHiring2portfolio.jpg"
             alt="Laravel Vacancy Website"
             class="  hover-blur-sm w-full h-auto transition-transform duration-500 group-hover:scale-110 filter  "
           />
