aba6e726d2061d7e14612adea0b6ad8563e89792
Daanfrie <daanfrie@gmail.com>
2025-06-20 13:25:56 +0200

    QoL improvements. tailwind beter geconfigd. hele html uit de js gehaald, js alleen voor logica. nav voor mobile verbeterd mar niet af

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ export default {
       fontFamily: {
         domine: ['Domine', 'serif'],
       },
+      colors: {
+
+        accentGold: {
+          50: '#faf9f7',
+          100: '#f5f2ed',
+          200: '#e8e1d4',
+          300: '#dbc9b1',
+          400: '#c9ad87',
+          500: '#b4a978', // Main gold color
+          600: '#a39666',
+          700: '#8a7d56',
+          800: '#71674a',
+          900: '#5c543d',
+        },
+      },
     },
   },
   plugins: [],
